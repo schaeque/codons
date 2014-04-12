c1a146b3b59a4e8dc959d7c347fc5eee77fbad3c
Manni Schäck <mannischaeck@gmail.com>
2014-04-13 00:34:22 +0200

    Analysis Manni reproduced

--- data/analysis.R
+++ data/analysis.R
@@ -15,3 +15,13 @@ cu$split.GAA.GAG <- cu$GAA + cu$GAG
 cu$percentage.AAA <- cu$AAA / cu$split.AAG.AAA * 100
 cu$percentage.CAA <- cu$CAA / cu$split.CAA.CAG * 100
 cu$percentage.GAA <- cu$GAA / cu$split.GAA.GAG * 100
+
+# Codon usage per 1000 for whole ORFome
+
+cu.orfome <- colSums(cu[,2:65]) / sum(cu$n) * 1000
+
+# Normalized splitbox usage
+
+cu$norm.AAA <- (1000 / cu$n) * cu$split.AAG.AAA * 100 / (cu.orfome['AAG'] + cu.orfome['AAA'])
+cu$norm.CAA <- (1000 / cu$n) * cu$split.CAA.CAG * 100 / (cu.orfome['CAA'] + cu.orfome['CAG'])
+cu$norm.GAA <- (1000 / cu$n) * cu$split.GAA.GAG * 100 / (cu.orfome['GAA'] + cu.orfome['GAG'])
